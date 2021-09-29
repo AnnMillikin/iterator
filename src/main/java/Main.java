73ccb12a225ecb3a62156ce5ce55788ac3518f41
Ann Millikin <amillikin@us.globalinfotek.com>
2021-09-29 07:38:59 -0400

    fix println values in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         int numItems = 6;
-        Item[] items = new Item[numItems];
+        main.java.Item[] items = new main.java.Item[numItems];
 
         // create new items
         for (int i = 0; i < numItems; i++) {
-            items[i] = new Item("item" + i);
+            items[i] = new main.java.Item("item" + i);
         }
 
         // create an Item Collection and iterator
-        List<Item> itemCollection = new ItemCollection(items);
-        Iterator<Item> iterator = itemCollection.iterator();
-
-        System.out.println("First item: " + iterator.currentItem());
-        System.out.println("Next item: " + iterator.next());
+        List<main.java.Item> itemCollection = new main.java.ItemCollection(items);
+        Iterator<main.java.Item> iterator = itemCollection.iterator();
 
         while (iterator.hasNext()) {
-            System.out.println("item: " + iterator.currentItem());
-            Item currentItem = iterator.currentItem();
+            main.java.Item currentItem = iterator.currentItem();
             System.out.println("current item: " + currentItem.getItemName());
             iterator.next();
         }
